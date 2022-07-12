3bf9d15c6f1ac69fce8e9603acb9ab7069591a32
Julian Cañaveral <julian_canaveral@Julians-MacBook-Pro.local>
2022-07-12 14:25:12 -0500

    react shop

---
+++ src/containers/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '@styles/Login.scss';
+
+import logo from '@logos/logo_yard_sale.svg';
+
+const Login = () => {
+    return (
+        <div className="login">
+            <div className="form-container">
+            <img src={logo} alt="logo" className="logo"></img>
+            <h1 className="title">Create a new password</h1>
+            <p className="subtitle">Enter a new passwrd for yue account</p>
+            <form action="/" className="form">
+                <label for="password" className="label">Password</label>
+                <input type="password" id="password" placeholder="*********" className="input input-password"></input>
+                <label for="new-password" className="label">Password</label>
+                <input type="password" id="new-password" placeholder="*********" className="input input-password"></input>
+                <input type="submit" value="Confirm" className="primary-button login-button"></input>
+            </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
